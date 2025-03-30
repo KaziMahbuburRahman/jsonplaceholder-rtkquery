bdfea44e99677480df86eec5b63476c1a9536986
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-30 15:57:13 +0600

    used hashrouter for github deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Feed from "./pages/Feed";
 import SinglePost from "./pages/SinglePost";
